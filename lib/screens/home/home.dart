dbae818c88e70a2962924d31550b5ed1d3b0dae4
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-09-12 14:51:32 +0530

    setup color values

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_testing/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,12 +9,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  // create a obj from AuthService
+  final AuthServices _auth = AuthServices();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-        title: Text("Home"),
+        title: const Text("Home"),
+          actions: [
+            ElevatedButton(onPressed: () async{
+              await _auth.signOut();
+            }, child: const Icon(Icons.logout))
+          ],
       ),)
     );
   }
