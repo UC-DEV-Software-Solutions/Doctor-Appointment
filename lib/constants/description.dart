aa1d6bb11395c435dad76a2c804e0c019a0d6623
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-09-12 18:12:02 +0530

    inserted a image in to sign in page

---
+++ lib/constants/description.dart
@@ -0,0 +1,1 @@
+const String description = "The Description";
---
+++ lib/constants/styles.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_testing/constants/colours.dart';
+import 'package:flutter/material.dart';
+
+const TextStyle descriptionStyle = TextStyle(
+  fontSize: 15,
+  color: textLight,
+  fontWeight: FontWeight.w200
+);
