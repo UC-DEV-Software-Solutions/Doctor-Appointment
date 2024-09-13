25054f1c5865c8a688f378f13358380509ca97c5
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-09-13 12:52:28 +0530

    Basic Sign in page Design

--- lib/screens/authentication/sign_in.dart
+++ lib/screens/authentication/sign_in.dart
@@ -1,5 +1,6 @@
 
 import 'package:firebase_testing/constants/colours.dart';
+import 'package:firebase_testing/constants/styles.dart';
 import 'package:firebase_testing/services/auth.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +14,14 @@ class Sign_In extends StatefulWidget {
 class _Sign_InState extends State<Sign_In> {
   // reference for the class
   final AuthServices _auth = AuthServices();
+
+  // Form Key
+  final _formKey = GlobalKey<FormState>();
+
+  // Email Password States
+  String email = "";
+  String password = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,29 +38,129 @@ class _Sign_InState extends State<Sign_In> {
         elevation: 0,
         backgroundColor: bgBlack,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            // Text(
-            //   description,
-            //   style: descriptionStyle,
-            // ),
-            Center(
-                child: Image.asset('assets/images/covid-19.png',
-                height: 500,)
-            ) ,
-            const Form(child: Column(
-              children: [
-                // email
-                // password
-                // google
-                // register
-                // button
-                // anonymous
-              ],        
-            ))
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15,right: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Text(
+              //   description,
+              //   style: descriptionStyle,
+              // ),
+              Center(
+                  child: Image.asset('assets/images/covid-19.png',
+                  height: 400,
+                  )
+              ) ,
+              const SizedBox(height: 10),
+              Form(
+                  key: _formKey,
+                  child: Column(
+                children: [
+                  // email
+                  TextFormField(
+                    validator: (value)=>
+                    value?.isEmpty == true ? "Enter a valid email" : null,
+                    onChanged: (value){
+                      setState(() {
+                        email = value;
+                      });
+                    },
+                  ),
+                  // password
+                  TextFormField(
+                    validator: (value)=>
+                    value!.length < 6 ? "Enter a valid password" : null,
+                    onChanged: (value){
+                      setState(() {
+                        password = value;
+                      });
+                    },
+                  ),
+                  // google
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                      "Login with social accounts",
+                      style: descriptionStyle
+                  ),
+                  GestureDetector(
+                    // Sign in with Google
+                    onTap: (){},
+                    child: Center(
+                        child: Image.asset('assets/images/GoogleIcon.png',
+                          height: 50,)
+                    ),
+                  ) ,
+                  const SizedBox(height: 20),
+                  // register
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                    const Text("Do not have an account?",
+                    style: descriptionStyle,
+                    ),
+                    const SizedBox(width: 10,),
+                    GestureDetector(
+                      // Land to the Register Page
+                      onTap:(){},
+                      child: const Text(
+                        "REGISTER",
+                        style: TextStyle(color: mainBlue,
+                        fontWeight: FontWeight.w900,
+                      ),
+                      ),
+                    )]
+                  ),
+                  const SizedBox(height: 20),
+                  // button
+                  GestureDetector(
+                    // Method For Login User
+                    onTap: (){},
+
+                    child: Container(
+                      height: 40,
+                        width: 200,
+                      decoration: BoxDecoration(
+                          color: mainBlue,
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(width: 2, color: mainYellow)),
+                      child: const Center(
+                          child: Text("LOG IN",
+                        style: TextStyle(color: Colors.white,
+                          fontWeight: FontWeight.w700),
+                      ),
+                      )
+                    ),
+                  ),
+                  const SizedBox(height: 15,),
+                  // anonymous
+                  GestureDetector(
+                    // Method For Login User
+                    onTap: (){},
+
+                    child: Container(
+                        height: 40,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            color: mainBlue,
+                            borderRadius: BorderRadius.circular(100),
+                            border: Border.all(width: 2, color: mainYellow)),
+                        child: const Center(
+                          child: Text("LOG IN AS GUEST",
+                            style: TextStyle(color: Colors.white,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        )
+                    ),
+                  )
+                ],
+              ))
+            ],
+          ),
         ),
       ),
     );
