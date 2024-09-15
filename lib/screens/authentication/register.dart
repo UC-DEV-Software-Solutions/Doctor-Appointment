cbbb70a64206d4eb456c91c71f866ab6b5df875a
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-09-15 22:27:31 +0530

    install image_picker and Set Network Image for Profile

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -56,9 +56,20 @@ class _RegisterState extends State<Register> {
                   // ),
                   Center(
                       child: Image.asset('assets/images/covid-19.png',
-                        height: 400,
+                        height: 100,
                       )
                   ) ,
+                  const SizedBox(height: 10),
+                  const Stack(
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundImage: NetworkImage(
+                            'https://images.unsplash.com/photo-1511367461989-f85a21fda167?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                        ),
+                      )
+                    ],
+                  ),
                   const SizedBox(height: 10),
                   Padding(
                     padding: const EdgeInsets.all(4.0),
