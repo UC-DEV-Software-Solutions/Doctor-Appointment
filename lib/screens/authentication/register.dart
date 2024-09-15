0fc2d9aa93ec737b0a1efe72d7cac742cac9fd69
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-09-15 22:41:36 +0530

    Change the Position of Add_a_image icon

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -68,7 +68,10 @@ class _RegisterState extends State<Register> {
                             'https://images.unsplash.com/photo-1511367461989-f85a21fda167?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
                         ),
                       ),
-                      Positioned(child: Container(
+                      Positioned(
+                        bottom: 0,
+                          right: 0,
+                          child: Container(
                         height: 40,
                         width: 40,
                         decoration: BoxDecoration(
