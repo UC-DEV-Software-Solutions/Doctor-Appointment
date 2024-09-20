572036e8d5d2d64532dd899e4ebae8765e89bf7f
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-09-21 00:27:09 +0530

    Setup 2 seconds timer for status banner

--- lib/widgets/statusBanner.dart
+++ lib/widgets/statusBanner.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class StatusBanner extends StatelessWidget {
+class StatusBanner extends StatefulWidget {
 
   final String? message;
   final bool isSuccess;
@@ -12,27 +14,53 @@ class StatusBanner extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<StatusBanner> createState() => _StatusBannerState();
+}
+
+class _StatusBannerState extends State<StatusBanner> {
+
+  bool _isVisible = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Start a timer that hides the banner after 3 seconds
+    if (widget.message != null) {
+      Timer(const Duration(seconds: 2), () {
+        if (mounted) {
+          setState(() {
+            _isVisible = false;
+          });
+        }
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
 
-    if (message == null) return const SizedBox.shrink(); // Hide widget if no message
+    if (widget.message == null || !_isVisible) {
+      return const SizedBox.shrink(); // Hide widget if no message
+    }
 
     return Container(
       padding: const EdgeInsets.all(8.0),
-      color: isSuccess ? Colors.green.withOpacity(0.1) : Colors.red.withOpacity(0.1),
+      color: widget.isSuccess ? Colors.green.withOpacity(0.1) : Colors.red.withOpacity(0.1),
       child: Row(
         children: [
           Icon(
-            isSuccess ? Icons.check_circle : Icons.error,
-            color: isSuccess ? Colors.green : Colors.red,
+            widget.isSuccess ? Icons.check_circle : Icons.error,
+            color: widget.isSuccess ? Colors.green : Colors.red,
           ),
           const SizedBox(width: 10),
           Expanded(
             child: Text(
-              message!,
+              widget.message!,
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
-                color: isSuccess ? Colors.green : Colors.red,
+                color: widget.isSuccess ? Colors.green : Colors.red,
               ),
             ),
           ),
